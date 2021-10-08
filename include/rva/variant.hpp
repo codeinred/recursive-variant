ecbd5794298dfd2682aeba5ba768c6f16e77d938
Alecto Irene Perez <perez.cs@pm.me>
2021-10-07 22:40:58 -0600

    Implemented recursive variant type
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/rva/variant.hpp
+++ include/rva/variant.hpp
@@ -1,6 +1,7 @@
 #ifndef RECURSIVE_VARIANT_AUTHORITY_VARIANT_HPP
 #define RECURSIVE_VARIANT_AUTHORITY_VARIANT_HPP
 #include <cstdint>
+#include <variant>
 
 namespace rva {
 /**
@@ -27,6 +28,32 @@ struct replace;
 template <class T, class Find, class Replace>
 using replace_t = typename replace<T, Find, Replace>::type;
 
+struct self_t {};
+
+template <class... T>
+class variant : public std::variant<replace_t<T, self_t, variant<T...>>...> {
+   public:
+    using base_type = std::variant<replace_t<T, self_t, variant<T...>>...>;
+    constexpr static bool
+        nothrow_swappable = std::is_nothrow_swappable_v<base_type>;
+
+    using base_type::base_type;
+
+    variant() = default;
+    variant(variant const&) = default;
+    variant(variant&&) = default;
+    variant& operator=(variant const&) = default;
+    variant& operator=(variant&&) = default;
+
+    using base_type::swap;
+    constexpr void swap(variant& other) noexcept(nothrow_swappable) {
+        base_type::swap(other);
+    }
+    base_type& get_base() & { return *this; }
+    base_type const& get_base() const& { return *this; }
+    base_type&& get_base() && { return *this; }
+    base_type const&& get_base() const && { return *this; }
+};
 
 template <class T, class Find, class Replace>
 struct replace {
