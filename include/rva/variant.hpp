54cf2d790d1e67a4d2a9f11077f3c0a6c8370fcd
Alecto Irene Perez <perez.cs@pm.me>
2021-10-07 19:34:11 -0600

    Added documentation for replace, replace_t
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/rva/variant.hpp
+++ include/rva/variant.hpp
@@ -3,15 +3,37 @@
 #include <cstdint>
 
 namespace rva {
+/**
+ * @brief replace is a template type used to implement replace_t. It provides
+ * member, a using declaration named `type`.
+ *
+ * @tparam T the type to transform.
+ * @tparam Find the type to find
+ * @tparam Replace the type to replace it with.
+ */
+template <class T, class Find, class Replace>
+struct replace;
+/**
+ * @brief replace is a template that takes a type T (which itself might be a
+ * template), and replaces all instances of *Find* with *Replace*. For example:
+ *
+ * - `replace_t<char, char, int>` -> `int`
+ * - `replace_t<std::vector<char>, char, int>` -> `std::vector<int>`
+ * - `replace_t<std::variant<int, std::vector<self_t>>, self_t, rva::variant<int,
+ * self_t>>` -> `std::variant<int, std::vector<rva::variant<int, self_t>>>`;
+ *
+ * @tparam T the type to transform.
+ * @tparam Find the type to find
+ * @tparam Replace the type to replace it with.
+ */
+template <class T, class Find, class Replace>
+using replace_t = typename replace<T, Find, Replace>::type;
+
 
 template <class T, class Find, class Replace>
 struct replace {
     using type = T;
 };
-
-template <class T, class Find, class Replace>
-using replace_t = typename replace<T, Find, Replace>::type;
-
 template <class Find, class Replace>
 struct replace<Find, Find, Replace> {
     using type = Replace;
